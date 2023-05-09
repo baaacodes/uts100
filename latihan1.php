43ad829aa5e654254346fc9649aa1fb61792d63d
baaacodes <airlanggamaulana07@gmail.com>
2023-05-09 23:15:04 +0700

    add latihan1

---
+++ latihan1.php
@@ -0,0 +1,59 @@
+<?php
+// array
+// variable yang dapat memiliki banyak nilai
+// Pengulangan pada array
+
+$angka = [3, 5, 12, 20, 11];
+?>
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+
+    <style>
+        .kotak {
+            width: 50px;
+            height: 50px;
+            background-color: red;
+            text-align: center;
+            line-height: 50px;
+            margin: 3px;
+            float: left;
+
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <?php for ($i = 0; $i < count($angka); $i++) { ?>
+        <div class="kotak"><?php echo $angka[$i]; ?></div>
+    <?php } ?>
+
+    <div class="clear"></div>
+
+    <?php foreach ($angka as $a) { ?>
+        <div class="kotak"><?php echo $a ?></div>
+    <?php } ?>
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+</html>
